7530555a0fb700fbbfe0037342e208bae15e37a7
jayak <kumarijaya127@gmail.com>
2024-04-21 21:22:00 +0530

    routes added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -8,7 +9,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.render("index");
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files: files });
+  });
+});
+
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+  // console.log(req.body);
 });
 
 app.listen(3000);
